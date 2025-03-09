4cf60549dfd43aa75139c4560efee9f5b3d6a94c
Stanislav Petrov <setthat5@gmail.com>
2025-03-09 14:41:58 +0300

    добавлен файл pytest

---
+++ test_abs.py
@@ -0,0 +1,14 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+ 
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+  
